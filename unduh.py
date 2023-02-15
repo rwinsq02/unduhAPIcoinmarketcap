0588895d0ae05ae0231a8952d97f35d04fe9995f
rwinsq02 <125362410+rwinsq02@users.noreply.github.com>
2023-02-15 09:43:51 +0700

    Update unduh.py

--- unduh.py
+++ unduh.py
@@ -1,40 +1,29 @@
-import requests
 
+
+
+    import requests
 import time
 
 while True:
-
     # URL endpoint untuk mengunduh data pasar kripto dari CoinMarketCap
-
     url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
     # Tambahkan parameter API key ke header permintaan
-
     headers = {
-
         "X-CMC_PRO_API_KEY": "7152156b-9a3a-452f-8970-d6559e95c368"
-
     }
 
-    # Lakukan permintaan API
-
-    response = requests.get(url, headers=headers)
-
-    # Periksa apakah permintaan berhasil dilakukan
-
-    if response.status_code == 200:
-
-        data = response.json()
-
-        # Lakukan operasi yang ingin dilakukan dengan data
-
-        print(data)
-
-    else:
-
-        print("Gagal mengambil data API dari CoinMarketCap")
+    parameters = { 'symbol': 'BTC', 'convert': 'IDR' }
+ response = requests.get(url, headers=headers, params=parameters) if response.status_code == 200: data = response.json() btc_price = data['data']['BTC']['quote']['IDR']['price'] print(f'Harga Bitcoin saat ini adalah: {btc_price} IDR') else: print('Gagal mengambil data API')
 
     # Tunggu selama setengah jam sebelum mengambil data lagi
+    time.sleep(120)
 
-    time.sleep(1800)
+
+
+        
+
+    
+
+    
 
