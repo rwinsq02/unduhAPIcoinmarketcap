5e503d27989b70b9775f1bab869553b42baa939c
rwinsq02 <125362410+rwinsq02@users.noreply.github.com>
2023-02-15 12:00:43 +0700

    Update unduh.py

--- unduh.py
+++ unduh.py
@@ -1,25 +1,28 @@
 import requests
+import json
 import time
 
+# set up the request parameters
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '7152156b-9a3a-452f-8970-d6559e95c368'
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'your-api-key'
 }
 parameters = {
-  'symbol': 'BTC',
-  'convert': 'IDR'
+    'symbol': 'BTC',
+    'convert': 'IDR'
 }
-while True:
-response = requests.get(url, headers=headers, params=parameters)
-if response.status_code == 200:
-    data = response.json()
-    btc_price = data['data']['BTC']['quote']['IDR']['price']
-    print(f'Harga Bitcoin saat ini adalah: {btc_price} IDR')
-else:
-    print('Gagal mengambil data API')
 
-time.sleep(120)
+# make a request to the API every 10 seconds and print the Bitcoin price
+while True:
+    response = requests.get(url, headers=headers, params=parameters)
+    if response.status_code == 200:
+        data = response.json()
+        btc_price = data['data']['BTC']['quote']['IDR']['price']
+        print(f'Harga Bitcoin saat ini adalah: {btc_price} IDR')
+    else:
+        print('Gagal mengambil data API')
+    time.sleep(10)  # wait for 10 seconds before making the next request
 	 
         
 
