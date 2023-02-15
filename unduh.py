2450e6004007d7d5f1736afd3f7578dbc9cf0c09
rwinsq02 <125362410+rwinsq02@users.noreply.github.com>
2023-02-15 13:59:08 +0700

    Update unduh.py

--- unduh.py
+++ unduh.py
@@ -19,7 +19,7 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="dataapicoinmarketcap.db"
+  database="dataapicoinmarketcap"
 )
 
 # make a request to the API every 10 seconds and save the Bitcoin price to the database
