bf549b47cea193c8dafe75d208cf04d009e3c3e3
rwinsq02 <125362410+rwinsq02@users.noreply.github.com>
2023-02-15 19:37:22 +0700

    Update unduh.py

--- unduh.py
+++ unduh.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import mysql.connector
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 # set up the request parameters
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
@@ -23,21 +23,27 @@ mydb = mysql.connector.connect(
   database="dataapicoinmarketcap"
 )
 
+# define function to get current timestamp
+def last_updated():
+    return int(time.time())
+
 # make a request to the API every 5 minutes and save the Bitcoin price to the database
 while True:
     response = requests.get(url, headers=headers, params=parameters)
     if response.status_code == 200:
         data = response.json()
-        btc_price = round(data['data']['BTC']['quote']['USD']['price'], 2)
+        btc_price = round(data['data']['BTC']['quote']['USD']['price'], 4)
         last_updated = data['data']['BTC']['quote']['USD']['last_updated']
-        last_updated = datetime.strptime(last_updated, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d %H:%M:%S')
+        last_updated_dt = datetime.strptime(last_updated, '%Y-%m-%dT%H:%M:%S.%fZ')
+        last_updated_dt += timedelta(hours=7) # convert to GMT+7
+        last_updated_str = last_updated_dt.strftime('%Y-%m-%d %H:%M:%S')
 
-        print(f'Harga Bitcoin saat ini adalah: {btc_price} USD pada {last_updated}')
+        print(f'Harga Bitcoin saat ini adalah: {btc_price} USD pada {last_updated_str}')
 
         # save the Bitcoin price to the database
         mycursor = mydb.cursor()
         sql = "INSERT INTO bitcoin_prices (price, last_updated) VALUES (%s, %s)"
-        val = (btc_price, last_updated)
+        val = (btc_price, last_updated_str)
         mycursor.execute(sql, val)
         mydb.commit()
         print(mycursor.rowcount, "record inserted.")
