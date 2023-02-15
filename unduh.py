5abdc9013153f6b4d0aecea639098ba63484afc7
rwinsq02 <125362410+rwinsq02@users.noreply.github.com>
2023-02-15 16:55:25 +0700

    Update unduh.py

--- unduh.py
+++ unduh.py
@@ -1,55 +1,91 @@
 import requests
-import json
+import mysql.connector
+from mysql.connector import Error
 import time
-import mysql.connector import Error
-from datetime import datetime
 
-# set up the request parameters
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+
 headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': '7152156b-9a3a-452f-8970-d6559e95c368'
-}
-parameters = {
-    'symbol': 'BTC',
-    
+
+    "Accepts": "application/json",
+
+    "X-CMC_PRO_API_KEY": "7152156b-9a3a-452f-8970-d6559e95c368"
+
 }
 
-# connect to the MySQL database
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="dataapicoinmarketcap"
-)
-      mycursor = mydb.cursor()
-# make a request to the API every 6 minute and save the Bitcoin price to the database
-while True:
-    try:
-    response = requests.get(url, headers=headers, params=parameters)
-if response.status_code == 200:
-    data = response.json()
-    btc_price = data['data']['BTC']['quote']['BTC']['price']
-    last_updated = data['data']['BTC']['quote']['BTC']['last_updated']
-    print(f"Nilai Bitcoin saat ini: {btc_price} ")
-    print(f"Waktu terakhir diperbarui: {datetime.fromisoformat(last_updated).strftime('%Y-%m-%d %H:%M:%S')}")
+parameters = {
+
+    "symbol": "BTC",
+
+    "convert": "none"
+
+}
+
+# Connect to the database
+
+try:
+
+    mydb = mysql.connector.connect(
+
+        host="localhost",
+
+        user="root",
+
+        password="",
+
+        database="dataapicoinmarketcap"
+
+    )
+
+    mycursor = mydb.cursor()
+
+    # Insert data into the table
+
+    sql = "INSERT INTO btc_price (price, last_updated) VALUES (%s, %s)"
+
     
-    print("Gagal memuat data nilai Bitcoin")
-        # save the Bitcoin price to the database
-         sql = "INSERT INTO btc_price (price, last_updated) VALUES (%s, %s)"
-    val = (btc_price, last_updated)
-        mycursor.execute(sql, val)
-        mydb.commit()
-        print(mycursor.rowcount, "record inserted.")
-   except Error as e:
-        print('Gagal mengambil data API')
-        finally:
+
+    while True:
+
+        # Get the Bitcoin data from the API
+
+        response = requests.get(url, headers=headers, params=parameters)
+
+        if response.status_code == 200:
+
+            data = response.json()
+
+            btc_price = data['data']['BTC']['quote']['USD']['price']
+
+            last_updated = data['data']['BTC']['quote']['USD']['last_updated']
+
+            print("Price of BTC is:", btc_price, "Last updated:", last_updated)
+
+            val = (btc_price, last_updated)
+
+            mycursor.execute(sql, val)
+
+            mydb.commit()
+
+            print(mycursor.rowcount, "record inserted.")
+
+        time.sleep(300) # Wait for 5 minutes
+
+except Error as e:
+
+    print("Error while connecting to MySQL", e)
+
+finally:
+
     if (mydb.is_connected()):
+
         mycursor.close()
+
         mydb.close()
+
         print("MySQL connection is closed")
-    time.sleep(360)  # wait for 6 minutes before making the next request
-        
+
+
 
     
 
